0388a903ecb2cfb9066b1bd562645dd8663ce1a7
Mike Wong <mdw362@nyu.edu>
2020-08-07 19:29:07 -0400

    Fix spacing, remove comments

--- benchmark.c
+++ benchmark.c
@@ -27,38 +27,30 @@ static int benchmark(void)
 	struct bpf_insn prog[] = {
 
 
-		BPF_MOV64_IMM(BPF_REG_1, 0), /* r0 = 0 */
-		BPF_STX_MEM(BPF_W, BPF_REG_10, BPF_REG_1, -4), /* *(u32 *)(fp - 4) = r0 */
+        BPF_MOV64_IMM(BPF_REG_1, 0), 
+        BPF_STX_MEM(BPF_W, BPF_REG_10, BPF_REG_1, -4), 
         BPF_MOV64_IMM(BPF_REG_1, 1),
         BPF_STX_MEM(BPF_W, BPF_REG_10, BPF_REG_1, -8),
-		BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_ktime_get_ns),
-        BPF_MOV64_REG(BPF_REG_6, BPF_REG_0),
-		BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),
-        BPF_ALU64_IMM(BPF_ADD, BPF_REG_2, -4),
-		BPF_MOV64_IMM(BPF_REG_1, 0), /* r0 = 0 */
-        BPF_LD_MAP_FD(BPF_REG_1, map_fd),
-		BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem),
-        BPF_JMP_IMM(BPF_JEQ, BPF_REG_0, 0, 1),
-		BPF_STX_MEM(BPF_W, BPF_REG_0, BPF_REG_6, 0), /* *(u32 *)(fp - 4) = r0 */
-
-
-
         BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_ktime_get_ns),
-		BPF_MOV64_REG(BPF_REG_6, BPF_REG_0),
-
-		BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),
+        BPF_MOV64_REG(BPF_REG_6, BPF_REG_0),
+        BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),
+        BPF_ALU64_IMM(BPF_ADD, BPF_REG_2, -4),
+        BPF_MOV64_IMM(BPF_REG_1, 0), 
+        BPF_LD_MAP_FD(BPF_REG_1, map_fd),
+        BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem),
+        BPF_JMP_IMM(BPF_JEQ, BPF_REG_0, 0, 1),
+        BPF_STX_MEM(BPF_W, BPF_REG_0, BPF_REG_6, 0), 
+        /* Instructions to benchmark go here */
+        BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_ktime_get_ns),
+        BPF_MOV64_REG(BPF_REG_6, BPF_REG_0),
+        BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),
         BPF_ALU64_IMM(BPF_ADD, BPF_REG_2, -8),
         BPF_LD_MAP_FD(BPF_REG_1, map_fd),
-
-		BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem),
-
+        BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem),
         BPF_JMP_IMM(BPF_JEQ, BPF_REG_0, 0, 1),
-
-		BPF_STX_MEM(BPF_W, BPF_REG_0, BPF_REG_6, 0), /* *(u32 *)(fp - 4) = r0 */
-
-
-		BPF_MOV64_IMM(BPF_REG_0, 0), 
-		BPF_EXIT_INSN(),
+        BPF_STX_MEM(BPF_W, BPF_REG_0, BPF_REG_6, 0), 
+        BPF_MOV64_IMM(BPF_REG_0, 0), 
+        BPF_EXIT_INSN(),
 
 	};
 
